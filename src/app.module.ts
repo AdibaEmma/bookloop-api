d7fdcbb722c40c6af44dff5a3a570c67693d984b
Emmanuel Adiba <eabaagah@gmail.com>
2025-10-26 23:44:22 +0000

    Register global filters and interceptors in app module
    - Registered HttpExceptionFilter globally via APP_FILTER
    - Registered ApiVersionInterceptor globally via APP_INTERCEPTOR
    - Registered ResponseInterceptor globally via APP_INTERCEPTOR
    - Imported LoggerModule as global module
    - Ensures consistent error handling and response format across all endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import databaseConfig from './config/database.config';
+import { LoggerModule } from './common/logger/logger.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ApiVersionInterceptor } from './common/interceptors/api-version.interceptor';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { UsersModule } from './modules/users/users.module';
 import { BooksModule } from './modules/books/books.module';
 import { ListingsModule } from './modules/listings/listings.module';
@@ -25,6 +30,8 @@ import { ExchangesModule } from './modules/exchanges/exchanges.module';
       }),
       inject: [ConfigService],
     }),
+    // Global logger
+    LoggerModule,
     // Feature modules
     UsersModule,
     BooksModule,
@@ -32,6 +39,20 @@ import { ExchangesModule } from './modules/exchanges/exchanges.module';
     ExchangesModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ApiVersionInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    },
+  ],
 })
 export class AppModule {}
