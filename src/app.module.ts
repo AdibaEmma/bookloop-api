00c35735a012e52fb539bed3aa1c7660759a6801
Emmanuel Adiba <eabaagah@gmail.com>
2025-10-26 23:17:49 +0000

    Update app module to import all feature modules
    - Imported UsersModule, BooksModule, ListingsModule, ExchangesModule
    - Configured TypeORM with database config
    - Set up global ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
+import { UsersModule } from './modules/users/users.module';
+import { BooksModule } from './modules/books/books.module';
+import { ListingsModule } from './modules/listings/listings.module';
+import { ExchangesModule } from './modules/exchanges/exchanges.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // Environment configuration
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+      envFilePath: '.env',
+    }),
+    // Database configuration
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+      inject: [ConfigService],
+    }),
+    // Feature modules
+    UsersModule,
+    BooksModule,
+    ListingsModule,
+    ExchangesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
